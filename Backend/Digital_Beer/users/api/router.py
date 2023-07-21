87276e1373595b2e419d185bde2aba02538140de
Adrian-ok <vargasadrianok@gmail.com>
2023-07-21 10:21:41 -0300

    add new route, auth login user

--- Backend/Digital_Beer/users/api/router.py
+++ Backend/Digital_Beer/users/api/router.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from users.api.views import UserApiViewSet, UserView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 router_user = DefaultRouter()
 
@@ -10,5 +11,6 @@ router_user.register(
 )
 
 urlpatterns = [
-    path('auth/me/', UserView.as_view())
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/me/', UserView.as_view()),
 ]
