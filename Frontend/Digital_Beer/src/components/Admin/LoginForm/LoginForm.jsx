2fc2e9d8182a0ffe2649b2f9131df602e782f351
Adrian-ok <vargasadrianok@gmail.com>
2023-07-21 10:05:40 -0300

    add validation form

--- Frontend/Digital_Beer/src/components/Admin/LoginForm/LoginForm.jsx
+++ Frontend/Digital_Beer/src/components/Admin/LoginForm/LoginForm.jsx
@@ -1,39 +1,62 @@
 import React from 'react'
 import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+//SECCION 5 CAP 30
+function initialValues() {
+    return {
+        email: '',
+        password: ''
+    }
+}
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required('Campo requerido'),
+    password: Yup.string().required('Campo requerido').min(5, 'Minimo 5 caracteres'),
+})
 
 export function LoginForm() {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validateOnChange: false,
+        validationSchema: validationSchema,
+        onSubmit: (formValue) => {
+            alert(JSON.stringify(formValue, null, 2));
+        }
+    })
+
     return (
         <>
             <h3 className="text-2xl font-semibold mb-4">Acceder</h3>
-            <form className="flex flex-col gap-4 w-full max-w-sm px-3">
+            <form className="flex flex-col gap-4 w-full max-w-sm px-3" onSubmit={formik.handleSubmit}>
                 <div>
-                    <Label
-                        htmlFor="email1"
-                        value="Your email"
-                    />
+                    <Label htmlFor="email1" value="Your email"/>
                     <TextInput
-                        id="email1"
+                        id="email"
                         placeholder="name@flowbite.com"
                         required
                         type="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        helperText={<span className='text-red-600'>{formik.errors.email}</span>}
                     />
                 </div>
                 <div>
-                    <Label
-                        htmlFor="password1"
-                        value="Your password"
-                    />
+                    <Label htmlFor="password1" value="Your password"/>
                     <TextInput
-                        id="password1"
+                        id="password"
                         required
                         type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        helperText={<span className='text-red-600'>{formik.errors.password}</span>}
                     />
                 </div>
                 <div className="flex items-center gap-2">
                     <Checkbox id="remember" />
-                    <Label htmlFor="remember">
-                        Remember me
-                    </Label>
+                    <Label htmlFor="remember"> Remember me </Label>
                 </div>
                 <Button type="submit">
                     Submit
