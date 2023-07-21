acbf7f66af085c2fafd06b3292b31525ab9f40db
Adrian-ok <vargasadrianok@gmail.com>
2023-07-21 14:28:36 -0300

    add toastify

--- Frontend/Digital_Beer/src/App.jsx
+++ Frontend/Digital_Beer/src/App.jsx
@@ -1,11 +1,24 @@
 
 import { Navigation } from './routes'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
 
   return (
     <>
       <Navigation />
+
+      <ToastContainer
+        position='bottom-center'
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+      />
     </>
   )
 }
