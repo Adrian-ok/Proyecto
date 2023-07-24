9f70be7cd8d48ed29479d2fc9edcdb0f78f19047
Adrian-ok <vargasadrianok@gmail.com>
2023-07-24 11:19:08 -0300

    add function login

---
+++ Frontend/Digital_Beer/src/context/AuthContext.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, createContext } from 'react'
+import { setToken, getToken } from '../api/token'
+import { useUser } from '../hooks/useUser'
+
+export const AuthContext = createContext({
+    auth: undefined,
+    login: () => null,
+    logout: () => null
+})
+
+export function AuthProvider({children}) {
+    //SECCION  5 CAP 35
+    const { getMe } = useUser()
+    const [auth, setAuth] = useState(undefined)
+
+    //SECCION 5 CAP 36
+    useEffect(() => {
+        (async () => {
+            const token = getToken()
+
+            if(token){
+                const me = await getMe(token)
+                setAuth({token, me})
+            }
+            else{
+                setAuth(null)
+            }
+        })()
+    }, [])
+
+    //SECCION  5 CAP 35
+    const login = async (token) => {
+        setToken(token)
+        const me = await getMe(token)
+        setAuth({token, me})
+    }
+
+    const valueContext = {
+        auth,
+        login,
+        logout: () => console.log('cerrando')
+    }
+
+    if(auth === undefined) return null
+
+    return(
+        <AuthContext.Provider value={valueContext}>{children}</AuthContext.Provider>
+    )
+}
