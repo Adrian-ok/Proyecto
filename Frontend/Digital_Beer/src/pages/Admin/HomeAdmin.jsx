3aa3f01d2e2cd10bed254a1d8cff38f39ce1dc92
Adrian-ok <vargasadrianok@gmail.com>
2023-07-24 11:44:01 -0300

    add button logout

--- Frontend/Digital_Beer/src/pages/Admin/HomeAdmin.jsx
+++ Frontend/Digital_Beer/src/pages/Admin/HomeAdmin.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useAuth } from '../../hooks'
 
 export function HomeAdmin() {
+  
+  const { logout } = useAuth()
+
   return (
-    <div>HomeAdmin</div>
+    <div>
+      <button onClick={() => logout()} className='bg-red-500 rounded p-1'>
+        Logout
+      </button>
+    </div>
   )
 }
