ff347e9c4040fbadd51115aa557c6dd7512ce497
Adrian-ok <vargasadrianok@gmail.com>
2023-07-25 16:18:40 -0300

    add menu const

--- Frontend/Digital_Beer/src/utils/constants.js
+++ Frontend/Digital_Beer/src/utils/constants.js
@@ -1,3 +1,14 @@
+import { HiHome, HiTag, HiShoppingBag, HiViewGrid } from 'react-icons/hi'
+import { LuHistory } from "react-icons/lu"
+
 export const URL_API = 'http://127.0.0.1:8000'
 
 export const TOKEN = 'token'
+
+export const MENUS = [
+    { name: 'Orders', link: '/admin', icon: HiHome },
+    { name: 'Tables', link: '/admin/tables', icon: HiViewGrid },
+    { name: 'Products', link: '/admin/products', icon: HiShoppingBag },
+    { name: 'Categories', link: '/admin/categories', icon: HiTag },
+    { name: 'History Payments', link: '/admin/history-payments', icon: LuHistory },
+]
