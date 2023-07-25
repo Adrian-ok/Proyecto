4bfc6c400766a2664580a9934071f935fa6f7e61
Adrian-ok <vargasadrianok@gmail.com>
2023-07-25 12:11:20 -0300

    add Hooks

--- Frontend/Digital_Beer/src/hooks/useUser.js
+++ Frontend/Digital_Beer/src/hooks/useUser.js
@@ -1,7 +1,15 @@
-import {getMeApi} from '../api/user'
+import { useState } from 'react'
+import { getMeApi, getUsersApi } from '../api/user'
+import { useAuth } from '../hooks'
 
 //SECCION  5 CAP 35
 export function useUser() {
+    
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [users, setUsers] = useState(null)
+    const { auth } = useAuth()
+
     const getMe = async (token) => {
         try {
             const response = await getMeApi(token)
@@ -11,7 +19,25 @@ export function useUser() {
         }
     }
 
+    //SECCION 6 CAP 43
+    const getUsers = async () => {
+        try {
+            setLoading(true)
+            const response = await getUsersApi(auth.token)
+            setLoading(false)
+            setUsers(response)
+        } catch (error) {
+            setLoading(false)
+            setError(error)
+        }
+    }
+
     return {
-        getMe
+        getMe,
+        getUsers,
+        loading,
+        error,
+        users
     }
 }
+
